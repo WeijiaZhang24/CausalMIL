c3766841e485c3c17bb3b6222b32332db42df552
Weijia Zhang <weijia.zhang.xh@gmail.com>
2022-09-25 12:00:04 +0800

    cleanup

--- models/networks_colormnist.py
+++ models/networks_colormnist.py
@@ -1,7 +1,40 @@
+''' network with set transformer as bag prior'''
+import sys 
+sys.path.append("..") 
+
 import torch
 import torch.nn as nn
 import torch.distributions as dist
-from util import get_bag_labels, reorder_y
+from util import reorder_y
+import torch.distributions as dist
+
+def map_bag_embeddings(zx_q, zy_q, bag_idx, list_g):
+    bag_latent_embeddings = torch.empty(zx_q.shape[0], zy_q.shape[1], requires_grad= True).to(torch.device('cuda'))
+    for _, g in enumerate(list_g):
+        group_label = g
+        samples_group = bag_idx.eq(group_label).nonzero().squeeze()
+        if samples_group.numel() >1 :
+            for index in samples_group:
+                bag_latent_embeddings[index] = zy_q[list_g.index(group_label)]
+        else:
+            bag_latent_embeddings[samples_group] = zy_q[list_g.index(group_label)]
+    return bag_latent_embeddings
+
+def deep_set_prior(encoded_u, bag_idx):
+    bag_encoded = []
+    list_bags_labels = []
+
+    bags = (bag_idx).unique()
+    for _, g in enumerate(bags):
+        bag_label = g.item()
+        samples_bag = bag_idx.eq(bag_label).nonzero().squeeze()
+        group_encoded =  encoded_u[samples_bag,:]
+        sum_encoded = torch.mean(group_encoded, dim = 0, keepdim=True)
+        bag_encoded.append(sum_encoded)
+        list_bags_labels.append(bag_label)
+
+    bag_encoded = torch.cat(bag_encoded, dim=0).to(torch.device('cuda'))
+    return bag_encoded, list_bags_labels
 
 class encoder_x(nn.Module):
     # qzx z_I ~ x
@@ -24,9 +57,9 @@ class encoder_x(nn.Module):
         H = self.feature_extractor(x)
         H = H.flatten(start_dim = 1)
         
-        instance_latent_space_mu = self.instance_mu(H)
-        instance_latent_space_logvar = self.instance_logvar(H)
-        return instance_latent_space_mu, instance_latent_space_logvar
+        # instance_latent_space_mu = self.instance_mu(H)
+        # instance_latent_space_logvar = self.instance_logvar(H)
+        return H
 
     
 class decoder_x(nn.Module):
@@ -46,6 +79,18 @@ class decoder_x(nn.Module):
         loc_img = self.de2(hidden3)
         loc_img = self.de3(loc_img)
         return torch.sigmoid(loc_img)
+
+
+class encoder_xnou(nn.Module):
+    def __init__(self, hidden_dims, latent_dim):
+        super(encoder_xnou, self).__init__()
+
+        self.latent_dim = latent_dim
+        self.mean = nn.Linear(512, latent_dim)
+        self.logvar = nn.Linear(512, latent_dim)
+
+    def forward(self, inputs):
+        return self.mean(inputs), self.logvar(inputs)
 
 
 class auxiliary_y_fixed(nn.Module):
@@ -72,13 +117,14 @@ class auxiliary_y_fixed(nn.Module):
                     M[iter_id, :] = loc_ins[instances_bag]
                     max_z_ins[iter_id, :] = z_ins[instances_bag,:]
                     max_instances[iter_id, :] = bag_instances[instances_bag,:]
-
+        
+        # prediction for the max instances, original max instances, latent for max_instances, prediction for all instances
         return M, max_instances, max_z_ins, loc_ins
 
 
-class cmil_mnist(nn.Module):
+class tmil_mnist(nn.Module):
     def __init__(self, args):
-        super(cmil_mnist, self).__init__()
+        super(tmil_mnist, self).__init__()
         self.cuda = args.cuda
         self.instance_latent_dim = args.instance_dim
         self.num_classes = args.num_classes
@@ -89,28 +135,32 @@ class cmil_mnist(nn.Module):
         self.aux_loss_multiplier_y = args.aux_loss_multiplier_y
         self.decoder_x= decoder_x(latent_dim=self.instance_latent_dim)
         self.encoder_x = encoder_x(latent_dim=self.instance_latent_dim)
+        self.encoder_xnou = encoder_xnou(latent_dim=self.instance_latent_dim, hidden_dims=None)
 
         self.auxiliary_y_fixed = auxiliary_y_fixed(self.instance_latent_dim, self.num_classes)
+        
+        self.deep_set_mean = nn.Linear(512,1)
+        self.deep_set_logvar = nn.Linear(512, 1)
+
     def forward(self, bag, bag_idx, bag_label):
-        # Encode
-        # encode instance latents
-        instance_mu, instance_logvar = self.encoder_x(bag)
+        # Encode bag prior with deep set 
+        intermediate_output = self.encoder_x(bag)
+        instance_mu, instance_logvar = self.encoder_xnou(intermediate_output)
         instance_std = instance_logvar.mul(0.5).exp_()
         qzx = dist.Normal(instance_mu, instance_std)
-        zx_q = qzx.rsample()  # [# of instances, instance_latent_dim]
-        
-        list_g = get_bag_labels(bag_idx)
+        zx_q = qzx.rsample()  # [# of instances, instance_latent_dim] 
 
-        # kl-divergence error for bag latent space should be KL( q(z_y|x) || p(z_y|y) )
-        #reorder by the same order as bag_latent_embeddings
-        reordered_y = reorder_y(bag_label, bag_idx, list_g).to(torch.device('cuda'))
-
-        # kl-divergence error for instance latent space
-        KL_zx =  0.5 * (instance_mu.pow(2) + instance_std.pow(2) - 2*torch.log(instance_std) - 1).mean()
+        bag_encoded, list_bags_labels = deep_set_prior(intermediate_output, bag_idx)
+        prior_mu = self.deep_set_mean(bag_encoded)
+        prior_logvar = self.deep_set_logvar(bag_encoded)
+        prior_std = prior_logvar.mul(0.5).exp_()
+        KL_loss =  0.5 * (prior_mu.pow(2) + prior_std.pow(2) - 2*torch.log(prior_std) - 1).sum(dim=-1).mean()
+        KL_loss = KL_loss +  0.5 * (instance_mu.pow(2) + instance_std.pow(2) - 2*torch.log(instance_std) - 1).mean()
         
         x_target = bag.flatten(start_dim = 1).contiguous()
-        y_hat, x_max, ins_hat, _ = self.auxiliary_y_fixed(zx_q, bag_idx, x_target)
+        y_hat, x_max, ins_hat, ins_hat_all = self.auxiliary_y_fixed(zx_q, bag_idx, x_target)
         loss = nn.BCEWithLogitsLoss(reduction = 'mean')
+        reordered_y = reorder_y(bag_label, bag_idx, list_bags_labels)
         auxiliary_loss_y= loss(y_hat.squeeze(), reordered_y)
         
         # only calculates the recon_loss for the maximum one per bag
@@ -118,7 +168,7 @@ class cmil_mnist(nn.Module):
         loss = nn.MSELoss(reduction = 'mean') 
         reconstruction_loss = loss(x_recon,x_max)        
         
-        return reconstruction_loss, KL_zx, auxiliary_loss_y
+        return reconstruction_loss, KL_loss, auxiliary_loss_y
 
     def loss_function(self, bag, bag_idx, bag_label, epoch):
         # supervised
@@ -126,26 +176,26 @@ class cmil_mnist(nn.Module):
             kl_divergence_coef = min([self.kl_divergence_coef, (epoch * 1.) / self.warmup])
         else:
             kl_divergence_coef = self.kl_divergence_coef
-        reconstruction_loss, KL_zx, auxiliary_y \
+        reconstruction_loss, KL_loss, auxiliary_y \
             = self.forward(bag, bag_idx, bag_label)
         
-        elbo = (  reconstruction_loss + kl_divergence_coef * KL_zx + self.aux_loss_multiplier_y * auxiliary_y )
-        return elbo, auxiliary_y, reconstruction_loss, KL_zx
+        elbo = (  reconstruction_loss + kl_divergence_coef * KL_loss + self.aux_loss_multiplier_y * auxiliary_y )
+        return elbo, auxiliary_y, reconstruction_loss, KL_loss
         
-    def get_encoding(self, bag, bag_idx, threshold=0.5, L=10):
+  
+    def get_encoding(self, bag, bag_idx):
         with torch.no_grad():
-            ins_loc, ins_scale = self.encoder_x.forward(bag)
-            
-        return ins_loc
+            intermediate_x = self.encoder_x(bag)            
+            xu_mu, _ = self.encoder_xnou(intermediate_x)
+        return xu_mu
     
     def classifier_ins(self, bag, bag_idx):
         with torch.no_grad():
-            ins_loc, _ = self.encoder_x.forward(bag)            
-            # zy_q_loc, zy_q_scale = self.encoder_y.forward(bag)
-            
+            intermediate_x = self.encoder_x(bag)            
+            x_mu, _ = self.encoder_xnou(intermediate_x)
+
             x_target = bag.flatten(start_dim = 1)
-            _, _, _, pred_ins = self.auxiliary_y_fixed(ins_loc, bag_idx, x_target)
-        # return pred_ins
+            _, _, _, pred_ins = self.auxiliary_y_fixed(x_mu, bag_idx, x_target)
         return torch.sigmoid(pred_ins)
 
 
